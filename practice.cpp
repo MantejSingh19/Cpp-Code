92161299eb20aadafbf0d02c5d2672e29070ff23
Mantej <mantejsudan60@gmail.com>
2023-08-15 22:32:08 +0530

    fifth commit

---
+++ practice.cpp
@@ -0,0 +1,161 @@
+//prime till n
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n,i,j,k,z;
+    cin>>n;
+   /* for(i=1;i<=n;i++)
+    {
+        for(j=2;j<i;j++)
+        {
+            if(i%j==0)
+            {
+                cout<<"Not Prime: "<<i<<endl;
+                break;
+            }
+        }
+        if(j==i)
+        {
+            cout<<"Prime: "<<j<<endl;
+        }
+    }
+    vector<int> sum={1,5,8,9};//dynamic array
+    int a[]={1,2,3,4,5};
+    //range based for loop
+    for(auto i:{1,4,56,7})
+    {
+        cout<<i<<endl;
+    };
+
+
+    for(i=0;i<n;i++)
+    {
+         k=n-1;
+         while(k>i)
+            {
+                cout<<" ";
+                k--;
+
+            }
+        for(j=0;j<=i;j++)
+        {
+           cout<<"*";
+
+        }
+        cout<<endl;
+    }
+
+
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+          cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=n;j>i;j--)
+        {
+          cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(i=0;i<n;i++)
+    {
+         k=n-1;
+         while(k>i)
+            {
+                cout<<" ";
+                k--;
+
+            }
+        for(j=0;j<=i;j++)
+        {
+           cout<<"*";
+
+        }
+        cout<<endl;
+    }
+    for(i=n;i>0;i--)
+    {
+         k=n-1;
+         while(k>=i)
+            {
+                cout<<" ";
+                k--;
+
+            }
+        for(j=0;j<i;j++)
+        {
+           cout<<"*";
+
+        }
+        cout<<endl;
+    }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
